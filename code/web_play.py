49b1075c14be93c57661130ceeff878fbc75adf1
Gilbert Yun <blueguitar1227@gmail.com>
2024-06-10 23:49:14 +0900

    until alphabeta agent

--- code/web_play.py
+++ code/web_play.py
@@ -1,9 +1,10 @@
 from dlgo.agent.naive import RandomBot
 from dlgo.minimax.depthprune import DepthPrunedAgent 
+from dlgo.minimax.alphabeta import AlphaBetaAgent
 from dlgo.httpfrontend.server import get_web_app
 
 
-agent = DepthPrunedAgent()
+agent = AlphaBetaAgent()
 # agent = RandomBot()
 web_app = get_web_app({'random': agent})
 web_app.run()
